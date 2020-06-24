45c0504472b1d7c45b66d4d78612d15c16d6dc1b
patelsam2007 <67134809+patelsam2007@users.noreply.github.com>
2020-06-23 21:38:08 -0400

    Update bledetect.js

--- apps/bledetect/bledetect.js
+++ apps/bledetect/bledetect.js
@@ -38,7 +38,7 @@ function scan() {
 
   NRF.findDevices(devices => {
     devices.forEach(device =>{
-      let deviceName = device.id.substring(0,17);
+      let deviceName = device.id.substring(0,17) + "-TEST";
 
       if (device.name) {
         deviceName = device.name;
